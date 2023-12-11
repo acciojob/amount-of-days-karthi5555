c5235b8a2dd079e9261ac8df62ebd93d1e3d003f
karthi5555 <105117281+karthi5555@users.noreply.github.com>
2023-12-11 11:52:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function daysOfAYear(year){
+    let isLeap = false;
+    if(year % 4 ===0 ){
+        //may be a leap year
+        if(year % 100 === 0){
+            //century year
+            return year % 400 === 0 ? 366 : 365;
+        }
+        return 366
+    }
+    return 365
+}
+let year = prompt("enter the year")
+let days = daysOfAYear(year);
+alert(days)
